aaae2b3e05d2f96d2f49bbaa38a7c854da39060f
Jesús Briceño <jesus.briceno-experis@farmaciasperuanas.pe>
2024-08-19 12:21:12 -0500

    Setup UnitySDK

---
+++ UnitySDK/unityLibrary/src/main/java/com/company/product/OverrideUnityActivity.java
@@ -0,0 +1,27 @@
+package com.company.product;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.unity3d.player.UnityPlayerActivity;
+
+public abstract class OverrideUnityActivity extends UnityPlayerActivity
+{
+    public static OverrideUnityActivity instance = null;
+
+    abstract protected void sendEvent(String eventName, String parameter);
+    abstract protected void goToBack();
+    abstract protected void showRewardDetail();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        instance = this;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        instance = null;
+    }
+}
